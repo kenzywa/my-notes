3c2973097ae6e4576523b0d5ca13f928688de146
Roman <shukailo99@gmail.com>
2021-03-18 23:07:17 +0300

    Исправлено сохранение и остальные баги

--- My Notes/SecondViewController.swift
+++ My Notes/SecondViewController.swift
@@ -3,47 +3,50 @@ import UIKit
 class SecondViewController: UIViewController {
     
     
-    let bigTextField = UITextField(frame: CGRect(x: 0, y: 0, width: 380, height: 500 ))
+    let bigTextField = UITextField(frame: CGRect(x: 0, y: 0, width: 350, height: 1600 ))
     var note : ListNotes?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         loadNotes()
-        self.view.backgroundColor = .systemBackground
+        self.view.backgroundColor = .systemGray4
         self.title = note?.title
         
-        let doneButton = UIBarButtonItem(barButtonSystemItem: .done,
-                                        target: self,
-                                        action: #selector(saveNotes))
+            let doneButton = UIBarButtonItem(title: "Готово",
+                                             style:  .done,
+                                             target: self,
+                                             action: #selector(saveNotes))
+                                          
         navigationItem.rightBarButtonItem = doneButton
         
-        bigTextField.center = CGPoint(x: 205,y: 120)
+        bigTextField.center = CGPoint(x: 180,y: 120)
         bigTextField.textAlignment = .natural
         bigTextField.font = bigTextField.font?.withSize(20)
-        bigTextField.backgroundColor = .systemYellow
+        bigTextField.backgroundColor = .systemGray4
         bigTextField.textColor = .black
+        bigTextField.font = UIFont(name: "Helvetica", size: 25)
         bigTextField.placeholder = "Ваша заметка"
+        bigTextField.keyboardType = .default
         self.view.addSubview(bigTextField)
-        
-        
     }
+    
     @objc func saveNotes() {
+        guard let id = note?.id else { return }
         let newnote = bigTextField.text
-        UserDefaults.standard.setValue(newnote, forKey: "NewNoteKeys")
+        UserDefaults.standard.setValue(newnote, forKey: id)
         UserDefaults.standard.synchronize()
         
         let alertSucces = UIAlertController(title: "Заметка сохранена", message: .none, preferredStyle: .alert)
-        
-        
+        alertSucces.addAction(UIAlertAction(title: "ОК", style: .default, handler: nil))
+        present(alertSucces, animated: true, completion: nil)
     }
+    
     func loadNotes() {
-
-        self.bigTextField.text = bigTextField.text
-        guard let notesData = UserDefaults.standard.value(forKey: "NewNoteKeys") as? Data else {
+        guard let id = note?.id else { return }
+        guard let notesData = UserDefaults.standard.string(forKey: id) else {
             return
         }
-        self.bigTextField.text = bigTextField.text
-        print(bigTextField.text)
+        bigTextField.text = notesData
     }
 }
 
